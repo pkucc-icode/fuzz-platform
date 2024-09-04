79cf77231f77e4a3f0846bab7d4b5c8e860ef0e3
ning <ning.huang.ridson@gmail.com>
2024-09-04 15:45:31 +0800

    feat: optimize upload ui

--- apps/web-ele/src/views/fuzz/open/index.vue
+++ apps/web-ele/src/views/fuzz/open/index.vue
@@ -6,13 +6,17 @@ import { Page } from '@vben/common-ui';
 import {
   ElButton,
   ElCard,
+  ElCol,
   ElForm,
   ElFormItem,
   ElInput,
   ElMessage,
+  ElRow,
   ElSpace,
+  ElUpload,
   type FormInstance,
   type FormRules,
+  type UploadUserFile,
 } from 'element-plus';
 import { Plus, Trash } from 'lucide-vue-next';
 
@@ -52,7 +56,6 @@ const form = reactive<RuleForm>({
 
 const rules = reactive<FormRules<RuleForm>>({
   name: [{ message: '请输入项目名', required: true, trigger: 'blur' }],
-  repoUrl: [{ message: '请输入Git地址', required: true, trigger: 'blur' }],
 });
 
 const ruleFormRef = ref<FormInstance>();
@@ -61,22 +64,22 @@ const loading = ref<boolean>(false);
 const onSubmit = async (formEl: FormInstance | undefined) => {
   if (!formEl) return;
 
-  await formEl.validate((valid, fields) => {
+  await formEl.validate(async (valid, fields) => {
     if (valid) {
-      console.warn('submit!', fields);
+      loading.value = true;
+      try {
+        await openFuzz(form);
+      } catch {
+        ElMessage.error('提交失败');
+      } finally {
+        loading.value = false;
+      }
+      ElMessage.success('操作成功');
     } else {
       console.error('error submit!', fields);
+      ElMessage.error('提交失败');
     }
   });
-  loading.value = true;
-  try {
-    await openFuzz(form);
-  } catch {
-    ElMessage.error('提交失败');
-  } finally {
-    loading.value = false;
-  }
-  ElMessage.success('操作成功');
 };
 
 const resetForm = (formEl: FormInstance | undefined) => {
@@ -97,6 +100,8 @@ const addCommand = () => {
     value: '',
   });
 };
+
+const fileList = ref<UploadUserFile[]>([]);
 </script>
 
 <template>
@@ -106,51 +111,76 @@ const addCommand = () => {
         ref="ruleFormRef"
         :model="form"
         :rules="rules"
-        class="mx-auto max-w-[800px]"
+        class="mx-auto mt-10 max-w-[900px]"
         label-width="auto"
       >
-        <ElFormItem label="项目名" prop="name">
-          <ElInput v-model="form.name" />
-        </ElFormItem>
-        <ElFormItem label="项目代码" prop="repoUrl">
-          <ElInput v-model="form.repoUrl" />
-        </ElFormItem>
-        <ElFormItem label="编译器">
-          <ElInput v-model="form.compiler" />
-        </ElFormItem>
-        <ElFormItem label="默认编译配置">
-          <ElInput v-model="form.compilerSettings" />
-        </ElFormItem>
-        <ElFormItem label="模糊器">
-          <ElInput v-model="form.fuzz" />
-        </ElFormItem>
-        <ElFormItem label="模糊测试时间">
-          <ElInput v-model="form.fuzzTime" />
-        </ElFormItem>
-        <ElFormItem label="模糊测试目标程序">
-          <ElInput v-model="form.fuzzTarget" />
-        </ElFormItem>
-        <ElFormItem
-          v-for="(item, index) in form.fuzzCommands"
-          :key="item.key"
-          label="模糊测试命令"
-        >
-          <ElSpace direction="horizontal">
-            <ElInput v-model="item.value" />
-            <ElButton
-              v-if="index === 0"
-              :icon="Plus"
-              color="#626aef"
-              type="primary"
-              @click="addCommand"
-            />
-            <ElButton
-              v-if="index !== 0"
-              :icon="Trash"
-              @click.prevent="removeCommand(item)"
-            />
-          </ElSpace>
-        </ElFormItem>
+        <ElRow :gutter="50">
+          <ElCol :span="12">
+            <ElFormItem label="项目名" prop="name">
+              <ElInput v-model="form.name" placeholder="请输入项目名" />
+            </ElFormItem>
+            <ElFormItem label="项目代码" prop="repoUrl">
+              <ElInput
+                v-model="form.repoUrl"
+                placeholder="请输入代码仓库地址"
+              />
+            </ElFormItem>
+            <ElFormItem label="or">
+              <ElUpload
+                v-model:file-list="fileList"
+                action="/upload"
+                class="upload-demo"
+              >
+                <ElButton color="#626aef" type="primary">点击上传代码</ElButton>
+              </ElUpload>
+            </ElFormItem>
+            <ElFormItem label="编译器">
+              <ElInput
+                v-model="form.compiler"
+                placeholder="Default llvm-clang"
+              />
+            </ElFormItem>
+          </ElCol>
+
+          <ElCol :span="12">
+            <ElFormItem label="默认编译配置">
+              <ElInput
+                v-model="form.compilerSettings"
+                placeholder="Default null"
+              />
+            </ElFormItem>
+            <ElFormItem label="模糊器">
+              <ElInput v-model="form.fuzz" placeholder="Default AFL++" />
+            </ElFormItem>
+            <ElFormItem label="模糊测试时间">
+              <ElInput v-model="form.fuzzTime" placeholder="Default 1day" />
+            </ElFormItem>
+            <ElFormItem label="模糊测试目标程序">
+              <ElInput v-model="form.fuzzTarget" placeholder="Default all" />
+            </ElFormItem>
+            <ElFormItem
+              v-for="(item, index) in form.fuzzCommands"
+              :key="item.key"
+              label="模糊测试命令"
+            >
+              <ElSpace direction="horizontal">
+                <ElInput v-model="item.value" placeholder="Default null" />
+                <ElButton
+                  v-if="index === 0"
+                  :icon="Plus"
+                  color="#626aef"
+                  type="primary"
+                  @click="addCommand"
+                />
+                <ElButton
+                  v-if="index !== 0"
+                  :icon="Trash"
+                  @click.prevent="removeCommand(item)"
+                />
+              </ElSpace>
+            </ElFormItem>
+          </ElCol>
+        </ElRow>
         <ElFormItem class="mt-10">
           <ElButton
             :loading="loading"
--- apps/web-ele/src/views/fuzz/web/index.vue
+++ apps/web-ele/src/views/fuzz/web/index.vue
@@ -6,17 +6,21 @@ import { Page } from '@vben/common-ui';
 import {
   ElButton,
   ElCard,
+  ElCol,
   ElForm,
   ElFormItem,
   ElInput,
   ElMessage,
+  ElRow,
   ElSpace,
+  ElUpload,
   type FormInstance,
   type FormRules,
+  type UploadUserFile,
 } from 'element-plus';
 import { Plus, Trash } from 'lucide-vue-next';
 
-import { webFuzz } from '#/api';
+import { openFuzz } from '#/api';
 
 interface FuzzCommand {
   key: number;
@@ -52,33 +56,30 @@ const form = reactive<RuleForm>({
 
 const rules = reactive<FormRules<RuleForm>>({
   name: [{ message: '请输入项目名', required: true, trigger: 'blur' }],
-  repoUrl: [{ message: '请输入Git地址', required: true, trigger: 'blur' }],
 });
 
 const ruleFormRef = ref<FormInstance>();
-
 const loading = ref<boolean>(false);
 
 const onSubmit = async (formEl: FormInstance | undefined) => {
   if (!formEl) return;
 
-  await formEl.validate((valid, fields) => {
+  await formEl.validate(async (valid, fields) => {
     if (valid) {
-      console.warn('submit!', fields);
+      loading.value = true;
+      try {
+        await openFuzz(form);
+      } catch {
+        ElMessage.error('提交失败');
+      } finally {
+        loading.value = false;
+      }
+      ElMessage.success('操作成功');
     } else {
       console.error('error submit!', fields);
+      ElMessage.error('提交失败');
     }
   });
-
-  loading.value = true;
-  try {
-    await webFuzz(form);
-  } catch {
-    ElMessage.error('提交失败');
-  } finally {
-    loading.value = false;
-  }
-  ElMessage.success('操作成功');
 };
 
 const resetForm = (formEl: FormInstance | undefined) => {
@@ -99,62 +100,90 @@ const addCommand = () => {
     value: '',
   });
 };
+
+const fileList = ref<UploadUserFile[]>([]);
 </script>
 
 <template>
-  <Page description="支持多种编程语言" title="Web Fuzz">
+  <Page description="支持多种编程语言" title="源码 Fuzz">
     <ElCard class="mb-5">
       <ElForm
         ref="ruleFormRef"
         :model="form"
         :rules="rules"
-        class="mx-auto max-w-[800px]"
+        class="mx-auto mt-10 max-w-[900px]"
         label-width="auto"
       >
-        <ElFormItem label="项目名" prop="name">
-          <ElInput v-model="form.name" />
-        </ElFormItem>
-        <ElFormItem label="项目代码" prop="repoUrl">
-          <ElInput v-model="form.repoUrl" />
-        </ElFormItem>
-        <ElFormItem label="编译器">
-          <ElInput v-model="form.compiler" />
-        </ElFormItem>
-        <ElFormItem label="默认编译配置">
-          <ElInput v-model="form.compilerSettings" />
-        </ElFormItem>
-        <ElFormItem label="模糊器">
-          <ElInput v-model="form.fuzz" />
-        </ElFormItem>
-        <ElFormItem label="模糊测试时间">
-          <ElInput v-model="form.fuzzTime" />
-        </ElFormItem>
-        <ElFormItem label="模糊测试目标程序">
-          <ElInput v-model="form.fuzzTarget" />
-        </ElFormItem>
-        <ElFormItem
-          v-for="(item, index) in form.fuzzCommands"
-          :key="item.key"
-          label="模糊测试命令"
-        >
-          <ElSpace direction="horizontal">
-            <ElInput v-model="item.value" />
-            <ElButton
-              v-if="index === 0"
-              :icon="Plus"
-              color="#626aef"
-              type="primary"
-              @click="addCommand"
-            />
-            <ElButton
-              v-if="index !== 0"
-              :icon="Trash"
-              @click.prevent="removeCommand(item)"
-            />
-          </ElSpace>
-        </ElFormItem>
+        <ElRow :gutter="50">
+          <ElCol :span="12">
+            <ElFormItem label="项目名" prop="name">
+              <ElInput v-model="form.name" placeholder="请输入项目名" />
+            </ElFormItem>
+            <ElFormItem label="项目代码" prop="repoUrl">
+              <ElInput
+                v-model="form.repoUrl"
+                placeholder="请输入代码仓库地址"
+              />
+            </ElFormItem>
+            <ElFormItem label="or">
+              <ElUpload
+                v-model:file-list="fileList"
+                action="/upload"
+                class="upload-demo"
+              >
+                <ElButton color="#626aef" type="primary">点击上传代码</ElButton>
+              </ElUpload>
+            </ElFormItem>
+            <ElFormItem label="编译器">
+              <ElInput
+                v-model="form.compiler"
+                placeholder="Default llvm-clang"
+              />
+            </ElFormItem>
+          </ElCol>
+
+          <ElCol :span="12">
+            <ElFormItem label="默认编译配置">
+              <ElInput
+                v-model="form.compilerSettings"
+                placeholder="Default null"
+              />
+            </ElFormItem>
+            <ElFormItem label="模糊器">
+              <ElInput v-model="form.fuzz" placeholder="Default AFL++" />
+            </ElFormItem>
+            <ElFormItem label="模糊测试时间">
+              <ElInput v-model="form.fuzzTime" placeholder="Default 1day" />
+            </ElFormItem>
+            <ElFormItem label="模糊测试目标程序">
+              <ElInput v-model="form.fuzzTarget" placeholder="Default all" />
+            </ElFormItem>
+            <ElFormItem
+              v-for="(item, index) in form.fuzzCommands"
+              :key="item.key"
+              label="模糊测试命令"
+            >
+              <ElSpace direction="horizontal">
+                <ElInput v-model="item.value" placeholder="Default null" />
+                <ElButton
+                  v-if="index === 0"
+                  :icon="Plus"
+                  color="#626aef"
+                  type="primary"
+                  @click="addCommand"
+                />
+                <ElButton
+                  v-if="index !== 0"
+                  :icon="Trash"
+                  @click.prevent="removeCommand(item)"
+                />
+              </ElSpace>
+            </ElFormItem>
+          </ElCol>
+        </ElRow>
         <ElFormItem class="mt-10">
           <ElButton
+            :loading="loading"
             color="#626aef"
             type="primary"
             @click="onSubmit(ruleFormRef)"
