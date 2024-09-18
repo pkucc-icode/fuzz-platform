7a5d8bb18e1fee96d2e0a4f476a37f97561b7844
ning <ning.huang.ridson@gmail.com>
2024-09-18 08:17:30 +0800

    feat: fuzz config json

--- apps/backend/api/fuzz/open.ts
+++ apps/backend/api/fuzz/open.ts
@@ -3,6 +3,7 @@ import { unAuthorizedResponse } from '~/utils/response';
 import prisma from '~/lib/prisma';
 
 import { exec, spawn } from 'child_process';
+import fs from 'fs/promises';
 import { promisify } from 'util';
 
 const execPromise = promisify(exec);
@@ -43,7 +44,42 @@ export default eventHandler(async (event) => {
 
   const { id } = project;
 
+  const data = {
+    "program_name": project.name,
+    "url": project.url,
+    "source_code_path": "",
+    "afl_fuzz_args": {
+      "task_id": 1,
+      "Default_compiler": {
+        "default": project.compiler,
+        "compile_setting": project.compile_setting
+      },
+      "fuzzer": project.fuzz,
+      "fuzz_time": project.fuzz_time,
+      "fuzz_target": [
+        fuzzTarget
+      ],
+      "CC_module": "",
+      "CXX_module": ""
+    }
+  };
+
+  const jsonString = JSON.stringify(data, null, 4); // 第三个参数4用于格式化输出
+  await writeFile(jsonString);
+
+
   execPromise(`bash run.sh > run-${id}.log 2>&1 &`);
 
   return useResponseSuccess(project);
 });
+
+
+
+async function writeFile(jsonString: string) {
+  try {
+      await fs.writeFile('sh/config.json', jsonString);
+      console.log('File written successfully');
+  } catch (err) {
+      console.error('Error writing file:', err);
+  }
+}
